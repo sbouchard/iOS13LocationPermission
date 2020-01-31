32e57eefc4d7ddd03455e7467c76307742d300a1
Sylvain Bouchard <sylvain@waizulabs.com>
2020-01-31 09:39:28 -0500

    Initial Commit

---
+++ TestLocalisation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestLocalisation
+//
+//  Created by Sylvain Bouchard on 2020-01-31.
+//  Copyright © 2020 waizulabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
